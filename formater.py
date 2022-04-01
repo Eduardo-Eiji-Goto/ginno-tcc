e2b850b123f7dd9e5400b80ae5682ff30b86eb0f
Eduardo Eiji Goto <eijigot000@gmail.com>
2022-04-01 16:26:07 -0300

    docs: First commit

--- formater.py
+++ formater.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+import sys
+
+# This formater exists because some of the datasets available had ';' to separate
+# columns, which wasn't accepted by pandas as a readable CSV format.
+
+# replaces ';' for ','
+def main(inFile, outFile = 'datasets/corrected.csv'):
+    f = open(inFile,'r')
+    correctedString = f.read().replace(';',',')
+    f.close()
+
+    f = open(outFile, 'w')
+    f.write(correctedString)
+    f.close()
 
 if __name__ == '__main__':
-    print('Hello World')
+    args = sys.argv
+    if len(args) == 2:
+        main(args[1])
+    if len(args) == 3:
+        main(args[1],args[2])
